55c0b683abc8d8ecd38cc0b455ea0b1a13fdb2de
Bartek <bmroczkowski@icloud.com>
2024-03-09 22:29:40 +0100

    Start preparing piece classes

---
+++ game/pieces.py
@@ -0,0 +1,37 @@
+class Piece:
+    """
+    Chess piece representation
+    """
+
+    def __init__(self):
+        pass
+
+
+class King(Piece):
+    def __init__(self):
+        super().__init__()
+
+
+class Queen(Piece):
+    def __init__(self):
+        super().__init__()
+
+
+class Bishop(Piece):
+    def __init__(self):
+        super().__init__()
+
+
+class Knight(Piece):
+    def __init__(self):
+        super().__init__()
+
+
+class Rook(Piece):
+    def __init__(self):
+        super().__init__()
+
+
+class Pawn(Piece):
+    def __init__(self):
+        super().__init__()
