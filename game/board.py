4425e3fe03edc954fd375cd9c22aad5d5886ea83
Bartek <bmroczkowski@icloud.com>
2024-03-20 23:10:51 +0100

    Draw 'dots' for valid moves. Improve board drawing.

--- game/board.py
+++ game/board.py
@@ -6,65 +6,61 @@ from pieces import King, Queen, Bishop, Knight, Rook, Pawn
 class Board:
     LIGHT_COLOR = 238, 238, 238
     DARK_COLOR = 116, 116, 116
-    SELECT_COLOR = 56, 220, 255
+
+    GRAY_COLOR = 200, 200, 200
+    RED_COLOR = 255, 50, 50
 
     def __init__(self, tile_size):
         self.tile_size = tile_size
-        self.current = None
+        self.load_assets()
 
         self.color = "b"
         self.board = [[None for _ in range(8)] for _ in range(8)]
 
         for row in [0, 7]:
-            color = "b" if row == 0  else "w"
+            color = "b" if row == 0 else "w"
+            assets = self.black_assets if row == 0 else self.white_assets
             pawn_row = row + (-1 if row == 7 else 1)
 
-            self.board[row][0] = Rook(row, 0, color)
-            self.board[row][1] = Knight(row, 1, color)
-            self.board[row][2] = Bishop(row, 2, color)
-            self.board[row][3] = Queen(row, 3, color)
-            self.board[row][4] = King(row, 4, color)
-            self.board[row][5] = Bishop(row, 5, color)
-            self.board[row][6] = Knight(row, 6, color)
-            self.board[row][7] = Rook(row, 7, color)
+            self.board[row][0] = Rook(row, 0, color, assets[4])
+            self.board[row][1] = Knight(row, 1, color, assets[3])
+            self.board[row][2] = Bishop(row, 2, color, assets[2])
+            self.board[row][3] = Queen(row, 3, color, assets[1])
+            self.board[row][4] = King(row, 4, color, assets[0])
+            self.board[row][5] = Bishop(row, 5, color, assets[2])
+            self.board[row][6] = Knight(row, 6, color, assets[3])
+            self.board[row][7] = Rook(row, 7, color, assets[4])
 
             for col in range(8):
-                self.board[pawn_row][col] = Pawn(pawn_row, col, color)
+                self.board[pawn_row][col] = Pawn(pawn_row, col, color, assets[5])
+
+        self.current = None
+        self.valid_moves = []
 
     def draw(self, win):
         for i in range(8):
             for j in range(8):
                 color = self.LIGHT_COLOR if (i + j) % 2 == 0 else self.DARK_COLOR
-                pygame.draw.rect(
-                    win,
-                    color,
-                    (
-                        j * self.tile_size,
-                        i * self.tile_size,
-                        self.tile_size,
-                        self.tile_size,
-                    ),
-                )
+                x = j * self.tile_size
+                y = i * self.tile_size
 
-        for row in self.board:
-            for piece in row:
-                if piece is None:
-                    continue
+                pygame.draw.rect(win, color, (x, y, self.tile_size, self.tile_size))
+                piece = self.board[i][j]
 
-                piece.draw(win)
-
-                if piece.selected:
-                    pygame.draw.rect(
-                        win,
-                        self.SELECT_COLOR,
-                        (
-                            piece.col * self.tile_size,
-                            piece.row * self.tile_size,
-                            self.tile_size,
-                            self.tile_size,
-                        ),
-                        5,
+                if (i, j) in self.valid_moves:
+                    color, radius, border = (
+                        (self.GRAY_COLOR, 12, 0)
+                        if piece is None
+                        else (self.RED_COLOR, self.tile_size * 0.35, 5)
                     )
+
+                    center_x = x + self.tile_size // 2
+                    center_y = y + self.tile_size // 2
+
+                    pygame.draw.circle(win, color, (center_x, center_y), radius, border)
+
+                if piece is not None:
+                    piece.draw(win, self.tile_size)
 
     def select(self, piece):
         if self.current:
@@ -72,10 +68,44 @@ class Board:
 
         if self.current == piece:
             self.current = None
+            self.valid_moves = []
             return
 
         piece.selected = True
         self.current = piece
+        self.valid_moves = piece.valid_moves(self.board)
 
     def move(self):
         pass
+
+    def load_assets(self):
+        self.white_assets = list(
+            map(
+                lambda img: pygame.transform.smoothscale(
+                    img, (self.tile_size, self.tile_size)
+                ),
+                [
+                    pygame.image.load("./assets/wk.png"),
+                    pygame.image.load("./assets/wq.png"),
+                    pygame.image.load("./assets/wb.png"),
+                    pygame.image.load("./assets/wn.png"),
+                    pygame.image.load("./assets/wr.png"),
+                    pygame.image.load("./assets/wp.png"),
+                ],
+            )
+        )
+        self.black_assets = list(
+            map(
+                lambda img: pygame.transform.smoothscale(
+                    img, (self.tile_size, self.tile_size)
+                ),
+                [
+                    pygame.image.load("./assets/bk.png"),
+                    pygame.image.load("./assets/bq.png"),
+                    pygame.image.load("./assets/bb.png"),
+                    pygame.image.load("./assets/bn.png"),
+                    pygame.image.load("./assets/br.png"),
+                    pygame.image.load("./assets/bp.png"),
+                ],
+            )
+        )
--- game/pieces.py
+++ game/pieces.py
@@ -1,53 +1,32 @@
 import pygame
 
-SCALE_SIZE = 96
-
-white_assets = list(
-    map(
-        lambda img: pygame.transform.smoothscale(img, (SCALE_SIZE, SCALE_SIZE)),
-        [
-            pygame.image.load("./assets/wk.png"),
-            pygame.image.load("./assets/wq.png"),
-            pygame.image.load("./assets/wb.png"),
-            pygame.image.load("./assets/wn.png"),
-            pygame.image.load("./assets/wr.png"),
-            pygame.image.load("./assets/wp.png"),
-        ],
-    )
-)
-
-black_assets = list(
-    map(
-        lambda img: pygame.transform.smoothscale(img, (SCALE_SIZE, SCALE_SIZE)),
-        [
-            pygame.image.load("./assets/bk.png"),
-            pygame.image.load("./assets/bq.png"),
-            pygame.image.load("./assets/bb.png"),
-            pygame.image.load("./assets/bn.png"),
-            pygame.image.load("./assets/br.png"),
-            pygame.image.load("./assets/bp.png"),
-        ],
-    )
-)
-
 
 class Piece:
     """
     Chess piece representation
     """
 
-    def __init__(self, row, col, color):
+    SELECT_COLOR = 56, 220, 255
+
+    def __init__(self, row, col, color, img):
         self.row = row
         self.col = col
 
         self.color = color  # w | b
+        self.img = img
         self.selected = False
 
-    def draw(self, win):
-        x = SCALE_SIZE * self.col
-        y = SCALE_SIZE * self.row
+        self.king = False
+        self.pawn = False
 
-        win.blit(self.img, (x, y))
+    def draw(self, win, tile_size):
+        win.blit(self.img, (tile_size * self.col, tile_size * self.row))
+
+        if not self.selected:
+            return
+
+        x, y = self.col * tile_size, self.row * tile_size
+        pygame.draw.rect(win, self.SELECT_COLOR, (x, y, tile_size, tile_size), 5)
 
     def set_pos(self, pos):
         self.row, self.col = pos
@@ -56,43 +35,52 @@ class Piece:
 class King(Piece):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.img = white_assets[0] if self.color == "w" else black_assets[0]
+        self.king = True
+
+    def valid_moves(self, board) -> tuple:
+        moves = ()
+
+        return moves
 
     def __repr__(self):
         return f"king {self.color} at [{self.row}; {self.col}]; {self.selected}"
 
 
 class Queen(Piece):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.img = white_assets[1] if self.color == "w" else black_assets[1]
+    def valid_moves(self, board) -> tuple:
+        moves = ()
+
+        return moves
 
     def __repr__(self):
         return f"queen {self.color} at [{self.row}; {self.col}]; {self.selected}"
 
 
 class Bishop(Piece):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.img = white_assets[2] if self.color == "w" else black_assets[2]
+    def valid_moves(self, board) -> tuple:
+        moves = ()
+
+        return moves
 
     def __repr__(self):
         return f"bishop {self.color} at [{self.row}; {self.col}]; {self.selected}"
 
 
 class Knight(Piece):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.img = white_assets[3] if self.color == "w" else black_assets[3]
+    def valid_moves(self, board) -> tuple:
+        moves = ()
+
+        return moves
 
     def __repr__(self):
         return f"knight {self.color} at [{self.row}; {self.col}]; {self.selected}"
 
 
 class Rook(Piece):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.img = white_assets[4] if self.color == "w" else black_assets[4]
+    def valid_moves(self, board) -> tuple:
+        moves = ()
+
+        return moves
 
     def __repr__(self):
         return f"rook {self.color} at [{self.row}; {self.col}]; {self.selected}"
@@ -101,7 +89,12 @@ class Rook(Piece):
 class Pawn(Piece):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.img = white_assets[5] if self.color == "w" else black_assets[5]
+        self.pawn = True
+
+    def valid_moves(self, board) -> tuple:
+        moves = ()
+
+        return moves
 
     def __repr__(self):
         return f"pawn {self.color} at [{self.row}; {self.col}]; {self.selected}"
